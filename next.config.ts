66533781749836cdccacf4dd9c7fd36a66933a4a
Subham Dutta <duttasubham10@gmail.com>
2026-08-04 16:55:38 +0530

    Remove GitHub Pages subpath configuration

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-
-  basePath: "/subhamdutta.github.io",
-  assetPrefix: "/subhamdutta.github.io",
-
   images: {
     unoptimized: true,
   },
