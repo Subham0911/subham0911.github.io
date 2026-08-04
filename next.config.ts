490d93fbfbcc0a27df65f333a2ea841fcd903cb4
Subham Dutta <duttasubham10@gmail.com>
2026-08-04 15:35:12 +0530

    Fix GitHub Pages asset paths

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+
+  basePath: "/subhamdutta.github.io",
+  assetPrefix: "/subhamdutta.github.io/",
+
   images: {
     unoptimized: true,
   },
